01c839d44ceae0150f6bb9c262433bf821366eb5
yonghwan.jeon <zeroknight82@gmail.com>
2017-07-25 22:50:51 +0900

    Update homework1

--- Pro_study/Solution/Pro_study/src/week/second/homework/homework1.java
+++ Pro_study/Solution/Pro_study/src/week/second/homework/homework1.java
@@ -7,17 +7,20 @@ import java.util.Scanner;
 public class homework1 {
 
 	static int T;
+	static int sum;
 	static int answer;
 	static int[][] data;
 
 	public static void main(String[] args) throws FileNotFoundException {
-		System.setIn(new FileInputStream("Insert file path"));
+		String path = "F:\\study\\gitHub\\doomsday\\Pro_study\\Solution\\Pro_study\\src\\week\\second\\homework\\homework1.txt";
+		System.setIn(new FileInputStream(path));
 		Scanner sc = new Scanner(System.in);
 		T = sc.nextInt();
 		int N = 0;
 
 		for (int test_case = 1; test_case <= T; test_case++) {
 			answer = 0;
+			sum = 0;
 			N = sc.nextInt();
 			data = new int[N][N];
 
@@ -27,6 +30,8 @@ public class homework1 {
 				}
 			}
 
+			printArrary(N);
+			backtracking(0);
 			System.out.println("#" + test_case + " " + answer);
 		}
 
@@ -39,10 +44,13 @@ public class homework1 {
 	public static void printArrary(int N) {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				System.out.print(data[i][j]);
+				System.out.print(data[i][j]+" ");
 			}
 			System.out.println();
 		}
+	}
+	
+	public static void backtracking(int i){
 	}
 
 }
